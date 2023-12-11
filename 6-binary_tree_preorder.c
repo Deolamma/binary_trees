4ce5dff0719e473d061d13476d1c42ff2bed0232
Deolamma <adebanjoemmanuel01@gmail.com>
2023-12-11 10:17:29 +0100

    Fix: if tree is null, DO NOTHING

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,14 +11,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!(tree->left) && !(tree->right))
+	if (tree)
 	{
 		func(tree->n);
-		return;
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
-	func(tree->n);
-
-	binary_tree_preorder(tree->left, func);
-
-	binary_tree_preorder(tree->right, func);
 }
