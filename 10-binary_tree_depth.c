31338ed2a9e0a0af7178b8db6557e18305cf7f27
Deolamma <adebanjoemmanuel01@gmail.com>
2023-12-13 11:02:26 +0100

    Fix: handled case of if node is NULL

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,11 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t count;
 
-	/* if tree is NULL or we are at a leafnode */
-	if (!(tree->parent))
+	/* if tree is NULL or we are the root of the tree */
+	if (!tree || !(tree->parent))
 		return (0);
 
+	/* keep going up top until your reach the root */
 	count = binary_tree_depth(tree->parent);
 
 	return (count + 1);
