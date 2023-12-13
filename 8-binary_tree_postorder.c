91edb2fc2655951a706ebd8b689c8e67d3c4a5b4
Deolamma <adebanjoemmanuel01@gmail.com>
2023-12-13 09:56:33 +0100

    Update: refactored code

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,15 +14,13 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		if (tree->left)
-		{
-			binary_tree_postorder(tree->left, func);
-		}
+		/* Keep going left until "left leafnode" */
+		binary_tree_postorder(tree->left, func);
 
-		if (tree->right)
-		{
-			binary_tree_postorder(tree->right, func);
-		}
+		/* Keep going right until "right leafnode" */
+		binary_tree_postorder(tree->right, func);
+		
+		/* print node */
 		func(tree->n);
 	}
 }
