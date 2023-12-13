08551f416995985967f9edcdd3d97258dca72738
Deolamma <adebanjoemmanuel01@gmail.com>
2023-12-13 12:21:31 +0100

    added comments

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,10 +11,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t countL;
 	size_t countR;
 
+	/* if tree is NULL or doesnt't exist */
 	if (!tree)
 		return (0);
+	/* if node is a leaf node return 1 */
 	if (!(tree->left) && !(tree->right))
 		return (1);
+
+	/* go left then right */
 
 	countL = binary_tree_size(tree->left);
 	countR = binary_tree_size(tree->right);
